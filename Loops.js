e2b027e57be65e6238bfcc3364b674521e0a9332
YousseffAmin <168475250+YousseffAmin@users.noreply.github.com>
2024-06-09 20:22:55 -0400

    firstloop

---
+++ Loops.js
@@ -0,0 +1,68 @@
+//  for (let i=0; i<=100; i++){
+//      if(i%3 == 0&& i%5 ==0){
+//       console.log("fizzBuzz");
+//      } else if (i%5==0){
+//          console.log("Buzz"); }
+//          else if(i%3==0){
+//              console.log("fizz");
+//          }else if(i%3!=0 || i%5 != 0){
+//              console.log(i)
+//          }
+//  }
+
+
+
+
+
+
+
+//  const n =5;
+//  for(let i=n; i<100; i++){
+//      if(n==1)continue ;
+//      for(let j=n; j>1; j--){
+//         if(i%j==0);
+//     }
+
+//   console.log(i);
+
+//   break;
+
+//  }
+
+
+
+//  const str="ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
+//  let cells="";
+//  for(let i =0; i<str.length;i++){
+//     if(str[i] != "\n"){
+//          cells=x=cells+str[i];
+//      }
+//      else{
+//          cells=cells;
+//          for(let j=0; j<cells.length; j++)
+//              {
+//                  if(cells[j]!=","){
+//                     cell2=cell2+cells1[j];
+//                 }
+//                  if(cell1[j]==","){
+//                      cell2=cell2+" ";
+//                  }if(j==cell1.length-1){console.log(cell2);
+//                     cell2=""
+//                 }
+//              }
+//          }    
+     
+//       }
+
+
+
+
+
+
+
+
+
+
+
+
+
